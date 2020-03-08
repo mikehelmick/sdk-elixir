8adad705617d867be9aae69396dac6ab2fddb6d9
Mike Helmick <mike.helmick@gmail.com>
2020-03-08 09:34:13 -0700

    validation for required attributes

--- lib/cloudevents/event.ex
+++ lib/cloudevents/event.ex
@@ -20,6 +20,10 @@ defmodule CloudEvents.Event do
     extensions: %{}
   ]
 
+  def id(%CloudEvents.Event{id: id}), do: id
+  def source(%CloudEvents.Event{source: source}), do: source
+  def source_uri(%CloudEvents.Event{source: source}), do: URI.parse(source)
+
   @doc """
   Determines if a given `%Event{}` struct represents a valid CloudEvent.
 
@@ -28,8 +32,12 @@ defmodule CloudEvents.Event do
   """
   @spec validate(%CloudEvents.Event{}) :: :ok | {:error, [String.t()]}
   def validate(event) do
-    errors = [] |>
-      validate_id(event)
+    errors =
+      []
+      |> validate_id(event)
+      |> validate_source(event)
+      |> validate_specversion(event)
+      |> validate_type(event)
 
     case length(errors) do
       0 -> :ok
@@ -37,14 +45,49 @@ defmodule CloudEvents.Event do
     end
   end
 
+  ## Internal methods for validation.
+
   defp validate_id(errors, %CloudEvents.Event{id: id}) when is_nil(id) do
     errors ++ ["CloudEvents attribute `id` must be present"]
   end
+
   defp validate_id(errors, %CloudEvents.Event{id: id}) do
-    if String.valid?(id) && String.length(id) > 0 do
+    validate_string(errors, id, "CloudEvents attribute `id` must be a non-empty string")
+  end
+
+  defp validate_source(errors, %CloudEvents.Event{source: source}) when is_nil(source) do
+    errors ++ ["CloudEvents attribute `source` must be present"]
+  end
+
+  defp validate_source(errors, %CloudEvents.Event{source: source}) do
+    validate_string(errors, source, "CloudEvents attribute `source` must be a non-empty string")
+  end
+
+  defp validate_specversion(errors, %CloudEvents.Event{specversion: sv}) when is_nil(sv) do
+    errors ++ ["CloudEvents attribute `specversion` must be present"]
+  end
+
+  defp validate_specversion(errors, %CloudEvents.Event{specversion: sv}) do
+    if sv == @specversion do
       errors
     else
-      errors ++ ["CloudEvents attribute `id` must be a non-empty string"]
+      errors ++ ["CloudEvents attribute `specversion` must have the value of `1.0`"]
+    end
+  end
+
+  defp validate_type(errors, %CloudEvents.Event{type: type}) when is_nil(type) do
+    errors ++ ["CloudEvents attribute `type` must be present"]
+  end
+
+  defp validate_type(errors, %CloudEvents.Event{type: type}) do
+    validate_string(errors, type, "CloudEvents attribute `type` must be a non-empty string")
+  end
+
+  defp validate_string(errors, str, message) do
+    if String.valid?(str) && String.length(str) > 0 do
+      errors
+    else
+      errors ++ [message]
     end
   end
 end
--- test/cloudevents/event_test.exs
+++ test/cloudevents/event_test.exs
@@ -1,5 +1,4 @@
 defmodule CloudEvents.EventTest do
-
   use ExUnit.Case, async: true
 
   test "missing id" do
@@ -8,5 +7,69 @@ defmodule CloudEvents.EventTest do
     assert message == "CloudEvents attribute `id` must be present"
   end
 
+  test "invalid id, empty string" do
+    event = %CloudEvents.Event{id: "", specversion: "1.0", source: "foo", type: "bar"}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `id` must be a non-empty string"
+  end
 
+  test "invalid id, non string" do
+    event = %CloudEvents.Event{id: 42, specversion: "1.0", source: "foo", type: "bar"}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `id` must be a non-empty string"
+  end
+
+  test "missing source" do
+    event = %CloudEvents.Event{specversion: "1.0", id: "1", type: "bar"}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `source` must be present"
+  end
+
+  test "invalid source, empty string" do
+    event = %CloudEvents.Event{id: "1", specversion: "1.0", source: "", type: "bar"}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `source` must be a non-empty string"
+  end
+
+  test "invalid source, non string" do
+    event = %CloudEvents.Event{id: "42", specversion: "1.0", source: :test, type: "bar"}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `source` must be a non-empty string"
+  end
+
+  test "missing specversion" do
+    event = %CloudEvents.Event{id: "1", source: "foo", type: "bar", specversion: nil}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `specversion` must be present"
+  end
+
+  test "invalid specversion, empty string" do
+    event = %CloudEvents.Event{id: "42", specversion: "", source: "http://test", type: "bar"}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `specversion` must have the value of `1.0`"
+  end
+
+  test "invalid specversion, wrong version" do
+    event = %CloudEvents.Event{id: "42", specversion: "0.3", source: "http://test", type: "bar"}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `specversion` must have the value of `1.0`"
+  end
+
+  test "missing type" do
+    event = %CloudEvents.Event{id: "42", specversion: "1.0", source: "foo"}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `type` must be present"
+  end
+
+  test "invalid type, empty string" do
+    event = %CloudEvents.Event{id: "42", specversion: "1.0", source: "foo", type: ""}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `type` must be a non-empty string"
+  end
+
+  test "invalid type, non string" do
+    event = %CloudEvents.Event{id: "42", specversion: "1.0", source: "foo", type: :bar}
+    {:error, [message]} = CloudEvents.Event.validate(event)
+    assert message == "CloudEvents attribute `type` must be a non-empty string"
+  end
 end
